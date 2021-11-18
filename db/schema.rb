6948e6e37b4e1f60b351f7a369228eb948c6a2f2
swaggingprogram <ogura0908work@gmail.com>
2021-11-18 17:21:52 +0900

    モデルおよびカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_062727) do
+ActiveRecord::Schema.define(version: 2021_11_18_081742) do
+
+  create_table "codes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "code", null: false
+    t.integer "category_id", null: false
+    t.integer "genre_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_codes_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -25,4 +36,5 @@ ActiveRecord::Schema.define(version: 2021_11_18_062727) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "codes", "users"
 end
