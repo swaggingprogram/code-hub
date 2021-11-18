17b959c5bba9dfa86060e5d45e16d44b03855e23
swaggingprogram <ogura0908work@gmail.com>
2021-11-18 17:25:52 +0900

    バリデーションの設定

--- app/models/code.rb
+++ app/models/code.rb
@@ -1,2 +1,11 @@
 class Code < ApplicationRecord
+  validates :title, presence: true
+  validates :code, presence: true
+
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :genre_id, numericality: { other_than: 1, message: "can't be blank" }
+
+  belongs_to :user
+  
 end
