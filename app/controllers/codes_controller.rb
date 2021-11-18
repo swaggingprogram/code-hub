582a50aa5d1b8e7f8ad5228a5a61c6a3c71f65b0
swaggingprogram <ogura0908work@gmail.com>
2021-11-18 18:23:53 +0900

    ルーティングとアクションの設定

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -1,2 +1,23 @@
 class CodesController < ApplicationController
+  def index
+    @codes = Code.all.order("created_at DESC")
+  end
+
+  def new
+    @code = Code.new
+  end
+
+  def create
+    @code = Item.new(code_params)
+    if @code.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def code_params
+    params.require(:code).permit(:title, :codetext, :category_id, :genre_id).merge(user_id: current_user.id)
+  end
 end
--- app/models/code.rb
+++ app/models/code.rb
@@ -1,11 +1,13 @@
 class Code < ApplicationRecord
   validates :title, presence: true
-  validates :code, presence: true
+  validates :codetext, presence: true
 
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :genre_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :image, presence: true
 
   belongs_to :user
+  has_one_attached :image
   
 end
