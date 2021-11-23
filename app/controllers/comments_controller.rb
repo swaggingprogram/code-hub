1eda466b0a25dcb532eae2bf49916d05a83b0e8b
swaggingprogram <ogura0908work@gmail.com>
2021-11-23 16:54:06 +0900

    コントローラーのアクションの設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to code_path(@comment.code)
+    else
+      @code = @comment.code
+      @comments = @code.comments
+      render "/codes/#{comment.code.id}"
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:concept).merge(user_id: current_user.id, code_id: params[:code_id])
+  end
 end
