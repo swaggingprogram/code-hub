f9fa770d15bc8c6369a6d0928b0f3ad73601debc
swaggingprogram <ogura0908work@gmail.com>
2021-11-23 14:37:26 +0900

    マイグレーションの設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_085814) do
+ActiveRecord::Schema.define(version: 2021_11_23_053238) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,16 @@ ActiveRecord::Schema.define(version: 2021_11_18_085814) do
     t.index ["user_id"], name: "index_codes_on_user_id"
   end
 
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "concept", null: false
+    t.bigint "user_id", null: false
+    t.bigint "code_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["code_id"], name: "index_comments_on_code_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -59,4 +69,6 @@ ActiveRecord::Schema.define(version: 2021_11_18_085814) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "codes", "users"
+  add_foreign_key "comments", "codes"
+  add_foreign_key "comments", "users"
 end
