5ac34412a0abcaa29ae0ab9a07c97c8a4290e09c
swaggingprogram <ogura0908work@gmail.com>
2021-11-29 16:31:17 +0900

    余分なコードの処分

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -50,9 +50,6 @@ class CodesController < ApplicationController
   end
 
   def search
-    if @q.blank?
-      redirect_to root_path
-    end
     @codes = @q.result
     category_id = params[:q][:category_id_eq]
     @category = Category.find_by(id: category_id)
