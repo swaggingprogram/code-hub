0393f5ae33ec65ed56d80e6d9a6659990a3b4927
swaggingprogram <ogura0908work@gmail.com>
2021-12-02 10:49:31 +0900

    コード単体テストの実装

--- spec/factories/codes.rb
+++ spec/factories/codes.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :code do
-    
+    title              { Faker::Name.initials(number: 6) }
+    codetext          { Faker::Lorem.sentence }
+    category_id       { Faker::Number.between(from: 2, to: 9) }
+    genre_id         { Faker::Number.between(from: 2, to: 4) }
+
+    association :user
   end
 end
--- spec/models/code_spec.rb
+++ spec/models/code_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Code, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @code = FactoryBot.build(:code)
+  end
+
+  describe 'コード新規投稿' do
+    context '新規投稿できる場合' do
+      it 'タイトル、コード内容、カテゴリー、ジャンルを入力すれば登録できる' do
+        expect(@code).to be_valid
+      end
+    end
+    context '新規投稿できない場合' do
+      it 'titleが空では登録できない' do
+        @code.title = ''
+        @code.valid?
+        expect(@code.errors.full_messages).to include("Title can't be blank")
+      end
+      it 'codetextが空では登録できない' do
+        @code.codetext = ''
+        @code.valid?
+        expect(@code.errors.full_messages).to include("Codetext can't be blank")
+      end
+      it 'categoryが空では登録できない' do
+        @code.category_id = ''
+        @code.valid?
+        expect(@code.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'categoryが1では登録できない' do
+        @code.category_id = '1'
+        @code.valid?
+        expect(@code.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'genreが空では登録できない' do
+        @code.genre_id = ''
+        @code.valid?
+        expect(@code.errors.full_messages).to include("Genre can't be blank")
+      end
+      it 'genreが1では登録できない' do
+        @code.genre_id = '1'
+        @code.valid?
+        expect(@code.errors.full_messages).to include("Genre can't be blank")
+      end
+    end
+  end
 end
