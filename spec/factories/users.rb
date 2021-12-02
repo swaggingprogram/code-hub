f60bb904cc3638318c31e5006cb5fefd68e201c8
swaggingprogram <ogura0908work@gmail.com>
2021-12-02 10:32:12 +0900

    ユーザー単体テストの実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              { Faker::Name.initials(number: 6) }
+    email                 { Faker::Internet.free_email }
+    password = ('a1' + Faker::Internet.password(min_length: 4))
+    introduction { Faker::Lorem.sentence }
+    password              { password }
+    password_confirmation { password }
   end
 end
