d0032f84add1f1c71d7c50d14950a27fbef858be
swaggingprogram <ogura0908work@gmail.com>
2021-12-02 12:16:41 +0900

    ユーザーの結合テストコードの実装

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-def basic_pass(path) #---❶
+def basic_pass(path)
   username = ENV["BASIC_AUTH_USER"] 
   password = ENV["BASIC_AUTH_PASSWORD"]
   visit "http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{path}"
@@ -57,4 +57,49 @@ RSpec.describe "Users", type: :system do
       expect(current_path).to eq user_registration_path
     end
   end
+end
+
+RSpec.describe 'ログイン', type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+  context 'ログインができるとき' do
+    it '保存されているユーザーの情報と合致すればログインができる' do
+      # トップページに移動する
+      visit root_path
+      # トップページにログインページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('ログインする')
+      # ログインページへ遷移する
+      visit new_user_session_path
+      # 正しいユーザー情報を入力する
+      fill_in 'email', with: @user.email
+      fill_in 'password', with: @user.password
+      # ログインボタンを押す
+      find('input[name="commit"]').click
+      # トップページへ遷移することを確認する
+      expect(current_path).to eq(root_path)
+      # カーソルを合わせるとログアウトボタンが表示されることを確認する
+      expect(page).to have_content('ログアウトする')
+      # サインアップページへ遷移するボタンやログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content("ログインする")
+      expect(page).to have_no_content("新規登録はこちらから")
+    end
+  end
+  context 'ログインができないとき' do
+    it '保存されているユーザーの情報と合致しないとログインができない' do
+      # トップページに移動する
+      visit root_path
+      # トップページにログインページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('ログイン')
+      # ログインページへ遷移する
+      visit new_user_session_path
+      # ユーザー情報を入力する
+      fill_in 'email', with: ''
+      fill_in 'password', with: ''
+      # ログインボタンを押す
+      find('input[name="commit"]').click
+      # ログインページへ戻されることを確認する
+      expect(current_path).to eq(new_user_session_path)
+    end
+  end 
 end
