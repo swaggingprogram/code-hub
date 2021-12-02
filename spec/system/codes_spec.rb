768b1677c241932297f794e68cc0da9770c058cd
swaggingprogram <ogura0908work@gmail.com>
2021-12-02 16:47:00 +0900

    編集の結合テストの実装

--- spec/system/codes_spec.rb
+++ spec/system/codes_spec.rb
@@ -54,3 +54,109 @@ RSpec.describe "Codes", type: :system do
   end
 end
 
+RSpec.describe 'ツイート編集', type: :system do
+  before do
+    @code1 = FactoryBot.create(:code)
+    @code2 = FactoryBot.create(:code)
+  end
+  context 'ツイート編集ができるとき' do
+    it 'ログインしたユーザーは自分が投稿したツイートの編集ができる' do
+      # ツイート1を投稿したユーザーでログインする
+      basic_pass root_path
+      visit new_user_session_path
+      fill_in "email", with: @code1.user.email
+      fill_in "password", with: @code1.user.password
+      find('input[name="commit"]').click
+      expect(current_path).to eq(root_path)
+      # ツイート1の詳細に遷移する
+      visit code_path(@code1)
+      # ツイート1に「編集」へのリンクがあることを確認する
+      expect(page).to have_link '編集', href: edit_code_path(@code1)
+      # 編集ページへ遷移する
+      visit edit_code_path(@code1)
+      # すでに投稿済みの内容がフォームに入っていることを確認する
+      expect(
+        find_by_id('code').value
+      ).to eq(@code1.codetext)
+      expect(
+        find_by_id('code_title').value
+      ).to eq(@code1.title)
+      expect(
+        find_by_id('code_category_id').value
+      ).to eq("#{@code1.category_id}")
+      expect(
+        find_by_id('code_genre_id').value
+      ).to eq("#{@code1.genre_id}")
+      # 投稿内容を編集する
+      fill_in "code", with: "#{@code1.codetext}"
+      fill_in "code_title", with: "#{@code1.title}"
+      select Category.data[@code1.category_id - 1][:name], from: "code[category_id]"
+      select Genre.data[@code1.genre_id - 1][:name], from: "code[genre_id]"
+      # 編集してもTweetモデルのカウントは変わらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { Code.count }.by(0)
+      # トップページに遷移する
+      expect(current_path).to eq(root_path)
+      # トップページには先ほど変更した内容のツイートが存在することを確認する（タイトル）
+      expect(page).to have_content(@code_title)
+      # トップページには先ほど変更した内容のツイートが存在することを確認する（テキスト）
+      expect(page).to have_content(@code_text)
+    end
+  end
+  context 'ツイート編集ができないとき' do
+    it 'ログインしたユーザーは自分以外が投稿したツイートの編集画面には遷移できない' do
+      # ツイート1を投稿したユーザーでログインする
+      basic_pass root_path
+      visit new_user_session_path
+      fill_in "email", with: @code1.user.email
+      fill_in "password", with: @code1.user.password
+      find('input[name="commit"]').click
+      expect(current_path).to eq(root_path)
+      # ツイート2の詳細に遷移する
+      visit code_path(@code1)
+      # ツイート2に「編集」へのリンクがないことを確認する
+      expect(page).to have_no_link '編集', href: edit_code_path(@code2)
+    end
+    it 'ログインしていないとツイートの編集画面には遷移できない' do
+      # トップページにいる
+      basic_pass root_path
+      # ツイート1に「編集」へのリンクがないことを確認する
+      visit code_path(@code1)
+      expect(page).to have_no_link '編集', href: edit_code_path(@code1)
+      # ツイート2に「編集」へのリンクがないことを確認する
+      visit code_path(@code2)
+      expect(page).to have_no_link '編集', href: edit_code_path(@code2)
+    end
+  end
+end
+
+RSpec.describe 'ツイート削除', type: :system do
+  before do
+    @tweet1 = FactoryBot.create(:tweet)
+    @tweet2 = FactoryBot.create(:tweet)
+  end
+  context 'ツイート削除ができるとき' do
+    it 'ログインしたユーザーは自らが投稿したツイートの削除ができる' do
+      # ツイート1を投稿したユーザーでログインする
+      # ツイート1に「削除」へのリンクがあることを確認する
+      # 投稿を削除するとレコードの数が1減ることを確認する
+      # 削除完了画面に遷移したことを確認する
+      # 「削除が完了しました」の文字があることを確認する
+      # トップページに遷移する
+      # トップページにはツイート1の内容が存在しないことを確認する（画像）
+      # トップページにはツイート1の内容が存在しないことを確認する（テキスト）
+    end
+  end
+  context 'ツイート削除ができないとき' do
+    it 'ログインしたユーザーは自分以外が投稿したツイートの削除ができない' do
+      # ツイート1を投稿したユーザーでログインする
+      # ツイート2に「削除」へのリンクがないことを確認する
+    end
+    it 'ログインしていないとツイートの削除ボタンがない' do
+      # トップページに移動する
+      # ツイート1に「削除」へのリンクがないことを確認する
+      # ツイート2に「削除」へのリンクがないことを確認する
+    end
+  end
+end
